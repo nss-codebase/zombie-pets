f1ac2ad61fb152543d731cdbc5937e70346fc314
Chyld Medford <chyld.medford@gmail.com>
2014-09-09 11:43:46 -0500

    pets

--- client/index.js
+++ client/index.js
@@ -4,6 +4,32 @@
   angular.module('zombie-pets', [])
   .controller('MainController', ['$scope', function($scope){
     $scope.title = 'Zombie Pets';
+
+    $scope.weapon = {};
+    $scope.weapons = [];
+    $scope.pet = {health:100};
+    $scope.pets = [];
+
+    $scope.addWeapon = function(){
+      $scope.weapons.push($scope.weapon);
+      $scope.weapon = {};
+      $('#name').focus();
+    };
+
+    $scope.addPet = function(){
+      var index = $scope.pet.weapon * 1;
+      $scope.pet.weapon = $scope.weapons[index];
+      $scope.pets.push($scope.pet);
+      $scope.pet = {health:100};
+    };
+
+    $scope.toggleWeapon = function(){
+      $scope.hideWeapon = !!!$scope.hideWeapon;
+    };
+
+    $scope.togglePet = function(){
+      $scope.hidePet = !!!$scope.hidePet;
+    };
   }]);
 })();
 
