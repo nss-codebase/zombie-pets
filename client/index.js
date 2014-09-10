08977d1445eb1c3c172c097214f9a92201d48dca
Chyld Medford <chyld.medford@gmail.com>
2014-09-09 20:21:46 -0500

    done

--- client/index.js
+++ client/index.js
@@ -13,6 +13,24 @@
     $scope.player1 = null;
     $scope.player2 = null;
 
+    $scope.fight = function(){
+      var num = Math.floor(Math.random() * 2) + 1,
+          p1  = $scope['player' + num],
+          p2  = $scope['player' + (num === 1 ? 2 : 1)];
+
+      hit(p1, p2);
+      hit(p2, p1);
+    };
+
+    function hit(p1, p2){
+      var max  = p1.isZombie ? 3 : p1.weapon.damage,
+          hurt = Math.floor(Math.random() * (max + 1));
+
+      p2.health -= hurt;
+      p2.isZombie = p2.health <= 0;
+      p2.health = p2.isZombie ? 0 : p2.health;
+    }
+
     $scope.addWeapon = function(){
       $scope.weapons.push($scope.weapon);
       $scope.weapon = {};
